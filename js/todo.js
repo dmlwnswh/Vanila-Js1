afcf379e12c1a983fa4f45f5bac3d5f972677e34
dmlwnswh <126458928+dmlwnswh@users.noreply.github.com>
2023-04-01 13:58:55 +0900

    0401

--- js/todo.js
+++ js/todo.js
@@ -2,16 +2,45 @@ const toDoForm = document.getElementById('todo-form');
 const toDoInput = toDoForm.querySelector('input');
 const toDoList = document.getElementById('todo-list');
 
+const toDos = [];
+
+function deleteToDo(e) {
+    const li =e.target.parentElement; //target: clicked element 
+    li.remove();
+}   
+
+function saveToDos() {
+    localStorage.setItem('toDos',JSON.stringify(toDos));
+}
+function paintToDo(newToDo) {
+    //create li like background
+    const newLi = document.createElement('li');
+    // li inside span
+    const newLiSpan = document.createElement('span');
+    newLiSpan.innerText = newToDo;
+    const button = document.createElement('button');
+    button.innerText = '❌';
+    button.addEventListener('click',deleteToDo);
+    newLi.appendChild(newLiSpan);
+    newLi.appendChild(button);
+    toDoList.appendChild(newLi);
+}
+
+
+
 function handleToDoSubmit(e) {
     e.preventDefault();
     const newToDo = toDoInput.value;
     //Todo input delete
     toDoInput.value = '';
-
-    
+    toDos.push(newToDo);
+    paintToDo(newToDo); 
+    saveToDos();
 }
 
 
-
-
 toDoForm.addEventListener('submit',handleToDoSubmit);
+
+//save -> load from localStorage 
+//local storage can save only str not list
+//then we use JSON.stringify
